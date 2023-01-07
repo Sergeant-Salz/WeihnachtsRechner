9b10fee018c19c00411fb9a5c9e39c25f158213c
l-nn-rt <a.raklennart@gmail.com>
2023-01-08 00:23:41 +0100

    snow is returning

--- build/script.js
+++ build/script.js
@@ -132,12 +132,12 @@ function addHandlers() {
 function resize() {
     let ratio = window.devicePixelRatio || 1;
     __seed = _seed;
-    _width = 512;
-    _height = 1024;
+    _width = window.innerWidth * ratio;
+    _height = window.innerHeight * ratio;
     $canvas.setAttribute( 'width', _width );
     $canvas.setAttribute( 'height', _height );
-    $canvas.style.width = `${_width / ratio * 0.7}px`;
-    $canvas.style.height = `${_height / ratio * 0.7}px`;
+    $canvas.style.width = `${_width / ratio * 0.6}px`;
+    $canvas.style.height = `${_height / ratio * 0.6}px`;
     render();
 }
 
@@ -522,3 +522,104 @@ function drawMessage() {
 
     _c.globalCompositeOperation = 'source-over';
 }
+
+
+/* --- Snowflakes --- */
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 400,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#fff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+        },
+        "opacity": {
+            "value": 1,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 10,
+            "random": true,
+            "anim": {
+                "enable": false,
+            }
+        },
+        "line_linked": {
+            "enable": false,
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "bottom",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 0.5
+                }
+            },
+            "bubble": {
+                "distance": 100,
+                "size": 4,
+                "duration": 0.3,
+                "opacity": 1,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 100,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
